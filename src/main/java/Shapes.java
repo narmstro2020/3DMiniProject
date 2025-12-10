d066dc29610e528966598c67a5faa115952388e4
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-12-10 13:34:22 -0500

    Start of 3D Mini Project

---
+++ src/main/java/Shapes.java
@@ -0,0 +1,35 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.Node;
+
+public class Shapes extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+
+    @Override
+    protected void initInput() {
+        super.initInput();
+    }
+
+    @Override
+    protected void initGame() {
+
+    }
+
+    private Node[] makeShapes() {
+        return null; // TODO: replace this with better.
+    }
+
+    private Entity makeEntity(double x, double y, double z) {
+        return null; // TODO: replace this with better.
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
